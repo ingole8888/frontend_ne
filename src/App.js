0f06ae297dde7aa6408e6e78c1cf76f5663d2f58
ingole8888 <ingole8888@gmail.com>
2023-10-28 12:05:00 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import AddInvoice from './components/AddInvoice';
-import {  Route, Routes } from "react-router-dom";
+import {  Route, Routes , Router} from "react-router-dom";
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Router>
     <Routes>
         <Route path="/"  component={Dashboard} />
         <Route path="/addinvoice" component={AddInvoice} />
     </Routes>
+    </Router>
     </>
   );
 }
